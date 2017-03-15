20fe482002e83feb1de6a06a11310cca22ded323
Joshua <joseldridge15@gmail.com>
2017-03-15 00:48:24 -0400

    It's working, need to change the layout however.

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,50 +2,30 @@
  * Created by Eldridge on 3/14/2017.
  */
 import React from 'react'
-<<<<<<< HEAD
 import {Text, View} from 'react-native'
-=======
 import {Text} from 'react-native'
->>>>>>> origin/master
 import FilterLink from '../containers/FilterLink'
 
 class Footer extends React.Component {
-    render(){
-<<<<<<< HEAD
-    return (
-        <View style={{flexDirection: "row", justifyContent: "space-between"}}>
-        <FilterLink filter="SHOW_ALL">
-            All
-        </FilterLink>
 
-        <FilterLink filter="SHOW_ACTIVE">
-            Active
-        </FilterLink>
 
-        <FilterLink filter="SHOW_COMPLETED">
-            Completed
-        </FilterLink>
-        </View>
-    );
-=======
-    return (<Text>
-        Show:
-        {" "}
-        <FilterLink filter="SHOW_ALL">
-            All
-        </FilterLink>
-        {", "}
-        <FilterLink filter="SHOW_ACTIVE">
-            Active
-        </FilterLink>
-        {", "}
-        <FilterLink filter="SHOW_COMPLETED">
-            Completed
-        </FilterLink>
-    </Text>);
->>>>>>> origin/master
+    render() {
+        return (
+            <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+                <FilterLink filter="SHOW_ALL">
+                    All
+                </FilterLink>
+
+                <FilterLink filter="SHOW_ACTIVE">
+                    Active
+                </FilterLink>
+
+                <FilterLink filter="SHOW_COMPLETED">
+                    Completed
+                </FilterLink>
+            </View>
+        );
     }
 }
-
 
 export default Footer
